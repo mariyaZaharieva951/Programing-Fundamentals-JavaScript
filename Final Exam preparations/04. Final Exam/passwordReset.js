0cf92707750ab6e9e0569814b780e903e9dfb946
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-11-25 11:20:24 +0200

    Final exam
    New solution of task

--- Final Exam preparations/04. Final Exam/passwordReset.js
+++ Final Exam preparations/04. Final Exam/passwordReset.js
@@ -1,56 +1,57 @@
-function passwordReset (input) {
+function passwordReset(input) {
 
-    let passWord = input.shift().split('');
-    
-    
+    let passWord = input.shift();
+
+
     let i = 0;
     let line = input[i];
-    
-    while(line !== 'Done') {
+
+    while (line !== 'Done') {
         line = line.split(' ');
         let command = line.shift();
-        let pass = '';
-        
-        if(command == 'TakeOdd') {
-            for(let i = 0; i < passWord.length; i++){
-                let letter = passWord[i];
-                if(i % 2 !== 0) {
-                    pass += `${letter}`;
-                }     
+
+
+        if (command == 'TakeOdd') {
+            let pass = '';
+            for (let j = 1; j < passWord.length; j += 2) {
+                let letter = passWord[j];
+                pass += `${letter}`;
             }
             passWord = pass;
             console.log(passWord)
-        } else if(command === 'Cut') {
-            let[index,length] = line;
-            let start = passWord.substring(0,Number(index));
-            let end = passWord.substring ((Number(index)+ Number(length)));
-            passWord = start + end;
-            
+        } else if (command === 'Cut') {
+            let [index, length] = line;
+            // let start = passWord.substring(0,Number(index));
+            // let end = passWord.substring ((Number(index)+ Number(length)));
+            // passWord = start + end;
+            let text = passWord.substring(Number(index), Number(index) + Number(length))
+            passWord = passWord.replace(text, '');
+
             console.log(passWord)
-        } else if(command === 'Substitute') {
-            let[substring,substitute] = line;
-            if(!passWord.includes(substring)){
+        } else if (command === 'Substitute') {
+            let [substring, substitute] = line;
+            if (!passWord.includes(substring)) {
                 console.log('Nothing to replace!');
             } else {
-            while(passWord.includes(substring)){
-                passWord = passWord.replace(substring,substitute);   
-            } 
-            console.log(passWord);
+                while (passWord.includes(substring)) {
+                    passWord = passWord.replace(substring, substitute);
+                }
+                console.log(passWord);
+            }
         }
-        }
-    
+
         i++;
         line = input[i];
-    } 
-    console.log(`Your password is: ${passWord}`)
-    
     }
+    console.log(`Your password is: ${passWord}`)
 
-passwordReset ((["up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy",
-"TakeOdd",
-"Cut 18 2",
-"Substitute ! ***",
-"Substitute ? .!.",
-"Done"])
+}
+
+passwordReset((["up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy",
+    "TakeOdd",
+    "Cut 18 2",
+    "Substitute ! ***",
+    "Substitute ? .!.",
+    "Done"])
 
 )
