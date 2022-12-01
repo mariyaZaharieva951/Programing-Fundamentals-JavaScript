15bb5a234c6fcf2b8e8f043dc805efc1b35cfa58
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-01 12:45:59 +0200

    Replace task
    Replace

--- 03. Arrays/Arrays More - Exercises/rotateArray.js
+++ 03. Arrays/Arrays More - Exercises/rotateArray.js
@@ -4,17 +4,9 @@ let rotate = Number(array[array.length - 1]);
 array.pop();
 
 for(let i = 0; i < rotate ; i++) {
-    let firstElement = array[0];
-
-    for (let j = 0; j < array.length; j++)  {   
-    array[j] = array[j + 1]; 
-    }
-    array[array.length - 1] = firstElement;  
-    
-
+    let lastElement = array.pop();
+    array.unshift(lastElement); 
 }
-
-
 console.log(array.join(' '));
 }
 
