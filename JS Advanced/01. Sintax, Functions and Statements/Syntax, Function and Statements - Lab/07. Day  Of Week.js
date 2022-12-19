76de9db2be64c708f7c1175d481c3e49a0f63c15
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-19 16:49:17 +0200

    Day of Week
    Solution

---
+++ JS Advanced/01. Sintax, Functions and Statements/Syntax, Function and Statements - Lab/07. Day  Of Week.js
@@ -0,0 +1,21 @@
+function dayOfWeek(day) {
+
+    let number = 0;
+
+switch(day){
+    case 'Monday': number = 1; break;
+    case 'Tuesday': number = 2; break;
+    case 'Wednesday': number = 3; break;
+    case 'Thursday': number = 4; break;
+    case 'Friday': number = 5; break;
+    case 'Saturday': number = 6; break;
+    case 'Sunday': number = 7; break;
+
+    default: console.log('error');break;
+} 
+if(number > 0){
+console.log(number)
+}
+}
+
+dayOfWeek ('jkjh')
