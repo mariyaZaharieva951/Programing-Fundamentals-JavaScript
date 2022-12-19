60431b0803c1280f6702363ba9c39254203ff5ed
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-19 16:12:38 +0200

    String Length
    Solution

---
+++ JS Advanced/01. Sintax, Functions and Statements/Syntax, Function and Statements - Lab/02. String Length.js
@@ -0,0 +1,11 @@
+function stringLength(arg1, arg2, arg3) {
+
+let length = arg1.length + arg2.length + arg3.length;
+let average = length / 3
+
+console.log(Math.round(length));
+console.log(Math.round(average))
+
+}
+
+stringLength('chocolate', 'ice cream', 'cake')
