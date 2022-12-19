f2bf4f03043935097685af9f9a3eb17d58ba74d4
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-19 16:31:22 +0200

    Match Operations
    Solution

---
+++ JS Advanced/01. Sintax, Functions and Statements/Syntax, Function and Statements - Lab/04. Match Operations.js
@@ -0,0 +1,19 @@
+function matchOperations(num1, num2, operator) {
+
+    let result = 0;
+
+switch(operator){
+    case '+': result = num1 + num2; break;
+    case '-': result = num1 - num2; break;
+    case '*': result = num1 * num2; break;
+    case '/': result = num1 / num2; break;
+    case '%': result = num1 % num2; break;
+    case '**': result = num1 ** num2; break;
+}
+console.log(result)
+}
+
+matchOperations (5, 6, '**')
+
+
+//'+', '-', '*', '/', '%', '**'.
