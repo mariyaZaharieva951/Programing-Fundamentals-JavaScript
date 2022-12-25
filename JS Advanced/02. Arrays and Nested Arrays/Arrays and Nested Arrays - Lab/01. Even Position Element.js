685dfeac45bc2d0d614d3985c5e1b6a5f16b09df
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-25 22:28:59 +0200

    Even position element
    Solution

---
+++ JS Advanced/02. Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/01. Even Position Element.js
@@ -0,0 +1,16 @@
+function evenPositionElement(array) {
+
+    let result = [];
+
+for(let i = 0; i <array.length; i++){
+    if(i % 2 === 0){
+        let currentElement = array[i];
+        result.push(currentElement)
+    }
+    
+}
+console.log(result.join(' '))
+
+}
+
+evenPositionElement(['20', '30', '40', '50', '60'])
