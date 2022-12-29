ca3a65cf42edadadaf0408a8f52ed61e42d2b834
Mariya Zaharieva <mariqtrunkova@gmail.com>
2022-12-29 09:42:34 +0200

    Sort an array by 2 criteria
    Solution

---
+++ JS Advanced/02. Arrays and Nested Arrays/Arrays and Nested Arrays - Exercises/08. Sort An Array By 2 Criteria.js
@@ -0,0 +1,14 @@
+function sortBy2Criteria(array) {
+
+array.sort((a,b) =>
+a.length - b.length || a.localeCompare(b));
+
+console.log(array.join('\n'))
+}
+
+sortBy2Criteria(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+)
