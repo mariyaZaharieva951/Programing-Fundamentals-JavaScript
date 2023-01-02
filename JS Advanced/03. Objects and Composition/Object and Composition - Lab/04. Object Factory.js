0b61246b4415f95c8a778d93281d48d8746bf0d3
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-01-02 18:58:23 +0200

    Object factory
    Solution

---
+++ JS Advanced/03. Objects and Composition/Object and Composition - Lab/04. Object Factory.js
@@ -0,0 +1,49 @@
+function factory(library, orders) {
+
+let fullfiledOrders = [];
+
+
+for(let order in orders){
+   let fullfiledOrder = orders[order].template;  
+   for(let part of orders[order].parts){
+    
+   fullfiledOrder[part] = library[part];
+   }
+   fullfiledOrders.push(fullfiledOrder); 
+}   
+return fullfiledOrders
+}
+
+
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']      
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']      
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']      
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']      
+    }
+  ];
+  const products = factory(library, orders);
+  console.log(products);
+  
