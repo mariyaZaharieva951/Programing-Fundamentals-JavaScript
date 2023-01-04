8f080a5df66aba5c1988ae7e14bcce74ff191372
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-01-04 16:30:49 +0200

    Calorie object
    Solution

---
+++ JS Advanced/03. Objects and Composition/Object and Composition - Exercises/01. Calorie Object.js
@@ -0,0 +1,15 @@
+function calorieObject (input) {
+
+    let result = {};
+
+for(let i = 0; i < input.length; i+=2){
+    let food = input[i];
+    let calories = Number(input[i+1]);
+    
+    result[food] = calories;
+}
+console.log(result)
+
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
