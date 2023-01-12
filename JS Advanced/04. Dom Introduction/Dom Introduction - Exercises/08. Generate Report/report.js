155d4846105acc65ebd85b709876e933eb61b4f8
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-01-12 09:31:48 +0200

    Generate Report
    Solution

---
+++ JS Advanced/04. Dom Introduction/Dom Introduction - Exercises/08. Generate Report/report.js
@@ -0,0 +1,22 @@
+function generateReport() {
+    let data = Array.from(document.querySelectorAll('tbody tr'))
+    let inputElements = Array.from(document.querySelectorAll('thead th input'));
+    let output = document.getElementById('output')
+    let result = [];
+    
+ 
+    for(let j = 0; j < data.length; j++){
+        let object = {};
+
+        for(let i = 0; i < inputElements.length; i++){
+        let row = data[j];
+        let value = row.children[i].textContent;
+        if(inputElements[i].checked){
+            object[inputElements[i].name] = value;
+         }    
+    }
+    result.push(object)
+}
+output.value = JSON.stringify(result);
+
+}
