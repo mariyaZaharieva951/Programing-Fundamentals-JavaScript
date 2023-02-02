29bd9b9418710d0fc0b795a5f201f017b5f580e9
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-02-02 14:45:13 +0200

    Request validator
    Update

--- JS Advanced/07. Unit Testing and Error Handling/Unit Testing and Error Handling - Exercises/Request Validator.js
+++ JS Advanced/07. Unit Testing and Error Handling/Unit Testing and Error Handling - Exercises/Request Validator.js
@@ -5,25 +5,25 @@ let versions = ['HTTP/0.9','HTTP/1.0','HTTP/1.1','HTTP/2.0'];
 let uriPattern = /^[\w.]+$/g;
 let messagePettern = /^[^<>\\&\'\"]+$/g;
 
-if(!object.hasOwnProperty("method") && !methods.includes(object.method)){
+if(!(object.hasOwnProperty("method") && methods.includes(object.method))){
     throw new Error("Invalid request header: Invalid Method")
 }
-if(!object.hasOwnProperty("uri") && (object.uri =='*' || !uriPattern.test(object.uri))){
+if(!(object.hasOwnProperty("uri") && (object.uri === '*' || uriPattern.test(object.uri)))){
     throw new Error("Invalid request header: Invalid URI")
 }
-if(!object.hasOwnProperty("version") && !versions.includes(object.version)){
-    throw new Error("Invalid request header: Invalid Varison")
+if(!(object.hasOwnProperty("version") && versions.includes(object.version))){
+    throw new Error("Invalid request header: Invalid Version")
 }
-if(!object.hasOwnProperty("message") && (object.message == "" || !messagePettern.test(object.message))){
+if(!(object.hasOwnProperty("message") && (object.message == "" || messagePettern.test(object.message)))){
     throw new Error("Invalid request header: Invalid Message")
 }
 return object;
 
 }
 console.log(requestValidator({
-  method: 'GET',
+  method: 'POST',
   uri: 'svn.public.catalog',
-  version: 'HTTP/1.1',
-  message: ''
+  //version: 'HTTP/1.1',
+  message: 'rm -rf /*'
 }))
 
