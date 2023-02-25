53ea60f9e35baa65e54419ad88468de45ea1c422
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-02-25 08:45:20 +0200

    Github-Repos
    Solution

--- JS Applications/02. Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
+++ JS Applications/02. Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
@@ -1,18 +1,27 @@
 function loadRepos() {
-	let user = document.getElementById('username');
+	let user = document.getElementById('username').value;
 	let ulRepos = document.getElementById('repos');
-	let button = document.querySelector('button');
+	//let button = document.querySelector('button');
 
-	button.addEventListener('click', loadRepos);
+	//button.addEventListener('click', loadRepos);
 
-	function loadRepos(event) {
-		let url = `https://api.github.com/users/${user.value}`;
-		let htppRequest = new XMLHttpRequest;
-		htppRequest.addEventListener('readystatechange', function() {
-			if(htppRequest.readyState === 4 && htppRequest.status === 200 ) {
-				ulRepos.appendChild(htppRequest.responseText);
+	fetch(`https://api.github.com/users/${user}/repos`) //от този url ще вадим информацията
+		.then(function(response) {
+			if(response.ok === false) {    //ако респонса не е true, хвърляме грешка, като взимаме статуса и текста на респонса
+				throw new Error (`${response.status} ${response.statusText}`);
+			}
+			return response.json() //ако респонса е true, взимаме json на респонса
+		})
+		.then(function(data) {
+			ulRepos.innerHTML = "";
+			for(let repo of data) {
+				ulRepos.innerHTML += `<li><a href = "${repo.html_url}">${repo.full_name}</a></li>`
 			}
 		})
-	}
+		.then(function (error) {
+			ulRepos.innerHTML = `${error.message}`;
+		}) 
+
+	
 
 }
