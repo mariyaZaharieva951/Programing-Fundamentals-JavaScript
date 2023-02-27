e859d3737bde4d7baf07f02b4e1c806116f6ab3f
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-02-27 07:34:59 +0200

    Gighub-Commits
    Solution

--- JS Applications/02. Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
+++ JS Applications/02. Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,32 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let user = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let list = document.querySelector('#commits');
+    list.innerHTML = '';
+
+    fetch(`https://api.github.com/repos/${user}/${repo}/commits`)
+    .then((response) => {
+        console.log(response);
+        if(response.ok === false) {
+            let error = response.status;
+            return Promise.reject(`${error}`)
+            
+        }
+        return response.json();
+    })
+    .then((data) => data.forEach(x => {
+        factory('li', `${x.commit.author.name}:${x.commit.message}`,'commits');
+
+    }))
+    .catch((err) => factory('li', `Error ${err} (Not Found)`));
+
+
+    function factory(el,text,id) {
+        let element = document.createElement(el);
+        element.textContent = text;
+        element.id = id;
+        list.appendChild(element);
+        return element;
+        
+    }
 }
