1d4fff0be8842435f875538d1eae12a5786d687d
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-03 23:01:19 +0300

    Cubicle
    Storage

--- JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/index.js
+++ JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/index.js
@@ -7,11 +7,18 @@
 
 const express = require('express');
 const hbs = require('express-handlebars');
+
+const { init: storage } = require('./models/storage');
+
 const { catalog } = require('./controllers/catalog');
 const { about } = require('./controllers/about');
 const { create, post } = require('./controllers/create');
 const { details } = require('./controllers/details');
 const { notFound } = require('./controllers/notFound');
+
+starts();
+
+async function starts() {
 
 const port = 3000;
 const app = express();
@@ -19,6 +26,9 @@ const app = express();
 app.engine('hbs', hbs({extname: '.hbs'})) //задаваме разширението на файловете и функцията, на която подаваме настройка за разширението(по подразбиране е handelbars)
 app.set('view engine', 'hbs') //сетваме viewEngine да търси hbs разширение
 app.use('/static', express.static('static')) //middlewere подаваме пътя на статичните файлове
+app.use(express.urlencoded({extended: false}))
+app.use(await storage());
+
 
 app.get('/', catalog) //регистрираме пътя
 app.get('/about', about);
@@ -29,6 +39,6 @@ app.post('/create', post);
 app.all('*', notFound); //ако заявкта не засече някой от по-горе подадеите пътища, то приложението ще върне '404'
 
 
-
-
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+
+}
