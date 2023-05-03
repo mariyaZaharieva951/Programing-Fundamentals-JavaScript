dcfa788969fc0a6e2150b2edad088320c0a43ce1
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-03 23:44:49 +0300

    Cubicle
    Details

--- JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/controllers/catalog.js
+++ JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/controllers/catalog.js
@@ -1,6 +1,12 @@
 module.exports = {
-    catalog: (req,res) => {
+    catalog: async (req,res) => {
         //console.log(req.storage)
-        res.render('index', {title: 'Catalog Page'});   //изпраща отговор като изобразява конкретно view
+        const cubes = await req.storage.getAll();
+        console.log(cubes)
+        const ctx = {
+            title: 'Cubicle',
+            cubes
+        }
+        res.render('index', ctx);   //изпраща отговор като изобразява конкретно view
     }
 }
--- JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/controllers/details.js
+++ JS Web/JS Back-end/02. Express and Templating/Cubicle Worrshop/controllers/details.js
@@ -1,5 +1,18 @@
 module.exports = {
-    details: (req,res) => {
-        res.render('details', {title: 'Cubicle'})
+    details: async (req,res) => {
+        //console.log(req.params.id)
+        const cubeId = req.params.id;
+        const cube = await req.storage.getById(cubeId)
+        
+        if(cube == undefined) {
+            res.redirect('/404');
+        } else {
+            console.log(cube)
+            const ctx = {
+                title: 'Cubicle',
+                cube
+            }
+            res.render('details', ctx)
+        }
     }
 }
