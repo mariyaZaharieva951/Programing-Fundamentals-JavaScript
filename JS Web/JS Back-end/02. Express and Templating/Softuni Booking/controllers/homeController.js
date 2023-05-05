0c83bf7ab2696ae7ef489ddb77fb3cec65a764fa
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-05 09:48:22 +0300

    Booking
    About and default controllers

--- JS Web/JS Back-end/02. Express and Templating/Softuni Booking/controllers/homeController.js
+++ JS Web/JS Back-end/02. Express and Templating/Softuni Booking/controllers/homeController.js
@@ -5,5 +5,13 @@ router.get('/', (req,res) => {
         res.render('home')
     });
 
+router.get('/about', (req,res) => {
+    res.render('about', {title: 'About us'})
+});
 
-module.exports = router
+
+
+
+
+
+module.exports = router;
--- JS Web/JS Back-end/02. Express and Templating/Softuni Booking/index.js
+++ JS Web/JS Back-end/02. Express and Templating/Softuni Booking/index.js
@@ -3,7 +3,8 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
-const homeController = require('./controllers/homeController')
+const homeController = require('./controllers/homeController');
+const defaultController = require('./controllers/defaultController');
 
 const app = express();
 app.engine('.hbs', hbs.engine);
@@ -12,6 +13,11 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
 
-app.use(homeController)
+app.use('/', homeController);
+app.use('/about', homeController);
+
+
+
+app.use('*', defaultController) //закачаме го след всички контролери
 
 app.listen(3000, () => console.log('Server is running on port 3000 '))
