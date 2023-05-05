412e4966a6e8333255b667523af76adf3153c45d
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-05 09:34:05 +0300

    Booking
    Home controller

--- JS Web/JS Back-end/02. Express and Templating/Softuni Booking/index.js
+++ JS Web/JS Back-end/02. Express and Templating/Softuni Booking/index.js
@@ -3,6 +3,8 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
+const homeController = require('./controllers/homeController')
+
 const app = express();
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
@@ -10,5 +12,6 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
 
+app.use(homeController)
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.listen(3000, () => console.log('Server is running on port 3000 '))
