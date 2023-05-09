336e815cbdeb2105dc6c93d8ed152af057f92a54
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-09 19:06:16 +0300

    update
    create

--- JS Web/JS Back-end/02. Express and Templating/Softuni Booking/services/accomodationService.js
+++ JS Web/JS Back-end/02. Express and Templating/Softuni Booking/services/accomodationService.js
@@ -14,6 +14,8 @@ async function persist() {  //записва промените във файл�
     })
 }
 
+
+
 function getAll() {
     return data
 }
@@ -22,8 +24,30 @@ function getById(id) {
     return data.find(i => i.id == id);
 }
 
+async function create(roomData) {
+    const room = {
+        id: getId(),
+        name: roomData.name,
+        description: roomData.description,
+        city: roomData.city,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        imgUrl: roomData.imgUrl
+    }
+
+    data.push(room);
+    await persist();
+
+    return room;
+}
+
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16)).slice(-6)
+}
+
 
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 }
