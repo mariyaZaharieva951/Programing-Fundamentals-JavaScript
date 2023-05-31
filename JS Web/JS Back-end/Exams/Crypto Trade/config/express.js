f9e03e2b2b6b9dfe095627b6cffcc85155f9d991
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-05-31 14:52:40 +0300

    Config
    Config

---
+++ JS Web/JS Back-end/Exams/Crypto Trade/config/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+
+
+module.exports = (app) => {
+    app.engine('hbs', engine({
+        extname: '.hbs'
+    }));
+    app.set('view engine', 'hbs');
+    
+
+    app.use('/static', express.static('static'))
+
+}
---
+++ JS Web/JS Back-end/Exams/Crypto Trade/config/routes.js
@@ -0,0 +1,9 @@
+const homeController = require("../controllers/homeController")
+
+
+
+
+module.exports = (app) => {
+    app.use('/', homeController)
+
+}
