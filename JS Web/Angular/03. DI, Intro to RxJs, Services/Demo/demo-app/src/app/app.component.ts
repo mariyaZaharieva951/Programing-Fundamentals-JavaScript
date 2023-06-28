942e4d2c172f496ae4da4646eee1eae0845803f9
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-06-28 22:20:29 +0300

    DI
    Dependency injection

--- JS Web/Angular/03. DI, Intro to RxJs, Services/Demo/demo-app/src/app/app.component.ts
+++ JS Web/Angular/03. DI, Intro to RxJs, Services/Demo/demo-app/src/app/app.component.ts
@@ -16,3 +16,32 @@ export class AppComponent {
   }
 
 }
+
+class Wallet {
+  balance = 0;
+
+  constructor(balance: number) {
+    this.balance = balance
+  }
+}
+
+class Car{
+  model = '';
+  constructor(model: string) {
+    this.model = model;
+  }
+}
+
+
+class PersonBetter {
+  wallet: Wallet;
+  car: Car;
+  constructor(car: Car, wallet: Wallet) {
+    this.wallet = wallet;
+    this.car = car;
+  }
+}
+
+const ivansCar = new Car('BMW');
+const ivansWallet = new Wallet(1000);
+const ivanPersonBetter = new PersonBetter(ivansCar,ivansWallet);
---
+++ JS Web/Angular/03. DI, Intro to RxJs, Services/Demo/demo-app/src/app/type/JsonPlaceholderUser.ts
@@ -0,0 +1,27 @@
+interface User {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    adress: Address,
+    phone: string,
+    website: string,
+    company: Company
+  }
+
+  interface Company {
+    name: string,
+    catchPhrase: string,
+    bs: string
+  }
+
+  interface Address {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string
+    }
+  }
