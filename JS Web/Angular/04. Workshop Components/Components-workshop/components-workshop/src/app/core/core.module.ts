f907b3aabc3b32ef1ca35761660734027dc8093a
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-07-05 14:50:02 +0300

    Navigation bar
    Navigation component

--- JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/core/core.module.ts
+++ JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent
   ]
 })
 export class CoreModule { }
