41ccbc25dac1cf92d22a8129b878c08b060902d2
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-07-07 15:38:18 +0300

    Update Home page
    Update Home page

--- JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/shared/home/home.component.ts
+++ JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/shared/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  get isLogged() {
+    return this.userService.isLogged
+  }
+  constructor(private userService: UserService, private router: Router) {
+
+  }
 }
