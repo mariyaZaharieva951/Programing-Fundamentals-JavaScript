89d3418d69cd22d8b690c114357de747a2ad2870
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-07-14 16:02:42 +0300

    Guard
    guard

--- JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/shared/guards/auth.activate.ts
+++ JS Web/Angular/04. Workshop Components/Components-workshop/components-workshop/src/app/shared/guards/auth.activate.ts
@@ -1,27 +1,27 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { UserService } from "src/app/auth/user.service";
+import { Observable } from "rxjs"
 
 
 
 
-Injectable({
+@Injectable({
     providedIn: 'root'
-});
+})
 
 export class AuthActivate implements CanActivate {
-    constructor(private authServive: UserService) {
-        
-    }
+    constructor(private userService: UserService, private router: Router) {}
     
-    canActivate(router: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-        const required = router.data['required'];
-        if(required == undefined) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+       
+    const required = route.data['required'];
+    
+        if(required == undefined || this.userService.isLogged == required) {
             return true;
         }
-        if(this.authServive.isLogged == required) {
-            return true;
-        }
+        const returnUrl = route.url.map(u => u.path).join('/');
+        return this.router.createUrlTree([`/auth/login`], {queryParams: {returnUrl}});
     }
 
 }
