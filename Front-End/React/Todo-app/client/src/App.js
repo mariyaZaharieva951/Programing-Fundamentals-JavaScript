2173069f29d3a8c0587019773949663bbcd71797
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-10-25 23:07:51 +0300

    React todo-app
    Demo

---
+++ Front-End/React/Todo-app/client/src/App.js
@@ -0,0 +1,139 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      
+      <header classNameName="navigation-header">
+    <span classNameName="navigation-logo">
+      <img src="./static/images/todo-icon.png" alt="todo-logo"/>
+    </span>
+    <span className="spacer"></span>
+    <span className="navigation-description">Todo List</span>
+  </header>
+
+  
+  <main classNameName="main">
+
+    <section className="todo-list-container">
+      <h1>Todo List</h1>
+
+      <div className="add-btn-container">
+        <button className="btn">+ Add new Todo</button>
+      </div>
+
+      <div className="table-wrapper">
+
+        <div className="loading-container">
+          <div className="loading-spinner">
+            <span className="loading-spinner-text">Loading</span>
+          </div>
+        </div>
+
+    
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header-task">Task</th>
+              <th className="table-header-status">Status</th>
+              <th className="table-header-action">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr className="todo is-completed">
+              <td>Give dog a bath</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+           
+            <tr className="todo is-completed">
+              <td>Do laundry</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo">
+              <td>Vacuum floor</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo is-completed">
+              <td>Feed cat</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo">
+              <td>Change light bulbs</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo is-completed">
+              <td>Feed cat</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo">
+              <td>Change light bulbs</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo is-completed">
+              <td>Go to Store</td>
+              <td>Completed</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            
+            <tr className="todo">
+              <td>Fill gas tank</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </main>
+
+  
+  <footer classNameName="footer">
+    <p>Copyright © designed by Mihail Valkov</p>
+  </footer>
+
+    </div>
+  );
+}
+
+export default App;
