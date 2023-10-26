1183637ec127d0033b93c78b5f3053922125b717
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-10-26 16:35:53 +0300

    Workshop Components
    Fetch users

--- Front-End/React/Workshop Components/client/src/App.js
+++ Front-End/React/Workshop Components/client/src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { useEffect, useState } from 'react';
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
+import { UserSection } from './components/user-section/UserSection';
+
+const baseUrl = 'http://localhost:3005/api';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseUrl}/users`)
+    .then(res => res.json())
+    .then(result => {
+      // console.log(resutl)
+      setUsers(result.users)
+    })
+  }, [])
+
+  console.log(users)
+
   return (
     <div className="App">
       <Header/>
 
       <main className="main">
         <section className="card users-container">
+          <Search/>
 
+          <UserSection/>
         </section>
       </main>
 
