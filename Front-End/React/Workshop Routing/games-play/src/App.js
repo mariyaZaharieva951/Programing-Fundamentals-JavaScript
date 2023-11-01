5835e92d4d67611cdae683dbfe2d37ca1478f894
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-11-01 15:33:31 +0200

    Add routes and pages

--- Front-End/React/Workshop Routing/games-play/src/App.js
+++ Front-End/React/Workshop Routing/games-play/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login'
+import Register from './components/Register/Register';
+import Create from './components/Create/Create';
+import Catalog from './components/Catalog/Catalog';
 
 
 
@@ -14,106 +18,16 @@ function App() {
   <main id="main-content">
     <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/create" element={<Create/>}/>
+        <Route path="/games" element={<Catalog/>}/>
+
     </Routes>
     
   
   </main>
-  {/*Home Page*/}
  
-  {/* Login Page ( Only for Guest users ) */}
-  {/* <section id="login-page" className="auth">
-    <form id="login">
-      <div className="container">
-        <div className="brand-logo" />
-        <h1>Login</h1>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Sokka@gmail.com"
-        />
-        <label htmlFor="login-pass">Password:</label>
-        <input type="password" id="login-password" name="password" />
-        <input type="submit" className="btn submit" defaultValue="Login" />
-        <p className="field">
-          <span>
-            If you don't have profile click <a href="#">here</a>
-          </span>
-        </p>
-      </div>
-    </form>
-  </section> */}
-  {/* Register Page ( Only for Guest users ) */}
-  {/* <section id="register-page" className="content auth">
-    <form id="register">
-      <div className="container">
-        <div className="brand-logo" />
-        <h1>Register</h1>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="maria@email.com"
-        />
-        <label htmlFor="pass">Password:</label>
-        <input type="password" name="password" id="register-password" />
-        <label htmlFor="con-pass">Confirm Password:</label>
-        <input type="password" name="confirm-password" id="confirm-password" />
-        <input className="btn submit" type="submit" defaultValue="Register" />
-        <p className="field">
-          <span>
-            If you already have profile click <a href="#">here</a>
-          </span>
-        </p>
-      </div>
-    </form>
-  </section> */}
-  {/* Create Page ( Only for logged-in users ) */}
-  {/* <section id="create-page" className="auth">
-    <form id="create">
-      <div className="container">
-        <h1>Create Game</h1>
-        <label htmlFor="leg-title">Legendary title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          placeholder="Enter game title..."
-        />
-        <label htmlFor="category">Category:</label>
-        <input
-          type="text"
-          id="category"
-          name="category"
-          placeholder="Enter game category..."
-        />
-        <label htmlFor="levels">MaxLevel:</label>
-        <input
-          type="number"
-          id="maxLevel"
-          name="maxLevel"
-          min={1}
-          placeholder={1}
-        />
-        <label htmlFor="game-img">Image:</label>
-        <input
-          type="text"
-          id="imageUrl"
-          name="imageUrl"
-          placeholder="Upload a photo..."
-        />
-        <label htmlFor="summary">Summary:</label>
-        <textarea name="summary" id="summary" defaultValue={""} />
-        <input
-          className="btn submit"
-          type="submit"
-          defaultValue="Create Game"
-        />
-      </div>
-    </form>
-  </section> */}
   {/* Edit Page ( Only for the creator )*/}
   {/* <section id="edit-page" className="auth">
     <form id="edit">
